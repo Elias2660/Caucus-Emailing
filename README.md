1fb1ebd525c536076e1a9de8ca0aa7f5963b12cb
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-05-15 22:27:35 -0400

    initial_commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# JProm Email & Barcode Generator
+
+
+[link](https://realpython.com/python-generate-qr-code/)
+
+[link2](https://www.twilio.com/en-us/blog/sending-email-attachments-with-twilio-sendgrid-python)
+
+
+[link3](https://www.geeksforgeeks.org/how-to-generate-barcode-in-python/)
---
+++ tests/test1.py
@@ -0,0 +1,39 @@
+
+# using SendGrid's Python Library
+# https://github.com/sendgrid/sendgrid-python
+import os
+import dotenv
+import base64
+from PIL import Image
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import (Mail, Attachment, FileContent, FileName, FileType, Disposition)
+
+dotenv.load_dotenv("../local.env")
+
+message = Mail(
+    from_email=os.getenv("PERSONAL_EMAIL"),
+    to_emails=os.getenv("PERSONAL_EMAIL"),
+    subject='Sending with Twilio SendGrid is Fun',
+    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+
+with open("image.png", "rb") as test_image:
+    encoded_file = base64.b64encode(test_image.read()).decode("utf-8")
+    test_image.close()
+    
+
+attachedFile = Attachment(
+    FileContent(encoded_file),
+    FileName('attachment.png'),
+    FileType('image/png'),
+    Disposition('attachment')
+)
+message.attachment = attachedFile
+
+try:
+    sg = SendGridAPIClient(os.getenv('SENDGRID_API_KEY2'))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.message)
