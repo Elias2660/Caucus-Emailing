e3ed10f342561a23407310c3c5e8f9176ec775fe
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2024-05-22 22:22:15 -0400

    correct

--- tests/SendEmail.py
+++ tests/SendEmail.py
@@ -23,38 +23,28 @@ def sendEmail(
         from_email=sender,
         to_emails=sendee,
         subject="Jprom Test",
-        html_content=f"""<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="jcaucus.css">
-</head>
-<body>
-    <div class="text">
-        <div class="greeting">
-            Hi {name}, 
-     
+        html_content=f"""<html>
+    <head>
+      <meta charset="UTF-8">
+      <title>JProm Ticket</title>
+    </head>
+    <body style="font-family: 'Helvetica Neue', sans-serif; color: #0629B0; margin: 0; padding: 0; background-color: #FFFEF9;">
+      <div style="max-width: 500px; margin: 0 auto; padding: 50px;">
+        <div style="text-align: center; margin-bottom: 50px;">
+          <img src="https://github.com/Junior-Caucus-SU/TicketCatcher/blob/main/stuy_logo.png?raw=true" alt="Stuy Icon" style="max-width: 100px;">
         </div>
-               <br>
-        <div>
-            We're excited to confirm your registration for Stuyvesant Junior Prom 2024. We look forward to welcoming you to an unforgettable evening of fun.
-            Below is your private barcode for entry to the event. Please ensure you save this email as this barcode will be scanned for your admission. 
+        <div style="text-align: left; line-height: 1.2;">
+          <p style="font-size: larger;">Hi {name}!,</p>
+          <p style="font-size: larger;">We're excited to confirm your registration for Stuyvesant Junior Prom 2024. We look forward to welcoming you to an unforgettable evening of fun. Below is your private barcode for entry to the event. Please ensure you save this email as this barcode will be scanned for your admission.</p>
+          //REPLACE WITH BARCODE
+          <p style="margin-top: 40px; font-size: larger;">A follow-up email will be sent to you soon containing detailed information regarding the event. Until then!</p>
+          <div style="margin-top: 40px;">
+            <p style="font-size: larger;">Cheers,<br>Elias, Will, and the Junior Caucus</p>
+          </div>
         </div>
-        <br>
-        <div>
-
-        </div>
-        <div class="second-last">
-            A follow-up email will be sent to you soon containing detailed information regarding the event. Until then!
-        </div>
-        <div>
-            <br>
-            Cheers, <br>The Junior Caucus
-        </div>
-    </div>
-</body>
-</html>""",
+      </div>
+    </body>
+  </html>""",
     )
 
     CreateBarcode.createBarcode(code, f"{attachment_name}")
@@ -81,4 +71,4 @@ def sendEmail(
 
 
 if __name__ == "__main__":
-    sendEmail("flam0799@gmail.com", "yzhang50@stuy.edu", "Will", 2083480230980)
+    sendEmail("flam0799@gmail.com", "exu51@stuy.edu", "Will", 2083480230980)
